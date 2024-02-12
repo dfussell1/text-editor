f454bf2b9ca3f886953cf7813f948181801181a6
dfussell1 <daltonfussell1@gmail.com>
2024-02-12 00:12:01 -0600

    fixed error in database.js, manifested dist folder with contents

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   
   const jateDb = await openDB('jate', 1);
   
-  const tx = jateDb.transaction('jate', 'readWrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   
   const store = tx.objectStore('jate');
   
